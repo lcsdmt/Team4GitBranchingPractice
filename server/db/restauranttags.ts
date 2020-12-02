888237f314507b6dfc0c2f049b6d4a4040ab46a8
Ashley Webster <ashwebster74@gmail.com>
2020-12-01 18:56:39 -0600

    add restaurant tags

--- server/db/restauranttags.ts
+++ server/db/restauranttags.ts
@@ -1,13 +1,5 @@
 import { Query } from './index';
 
-/* const insert = (restaurantsid: number, tags: []) => {
-  tags.forEach(tag => {
-    Query(`
-    insert into businesstags (tagsid, restaurantsid)
-values(?, ?)`,
-    [restaurantsid, tag])
-  })
-}*/
 
 const oneRestaurantTag = (restaurantsid: any) => Query(`call spRestaurantTags(?)`, [restaurantsid]);
 
