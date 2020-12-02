c616709dbc20217b546594aa3c4b900b0218aaab
Ashley Webster <ashwebster74@gmail.com>
2020-12-02 07:50:14 -0600

    added restaurant tags

---
+++ server/config/development.ts
@@ -0,0 +1,13 @@
+export default {
+  mysql: {
+    connectionLimit: 10,
+    user: 'dbAdmin',
+    password: 'ManOnTheMoon',
+    host: 'localhost',
+    database: 'localhost',
+  },
+
+  auth: {
+    secret: 'rolltide',
+  }
+}
